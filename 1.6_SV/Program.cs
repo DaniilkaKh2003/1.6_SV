e7a6672d23b03e3f235496b50f0550a23e0c3b82
Даниил <@LAPTOP-EEPRU07S>
2022-06-07 21:24:28 +0700

    Добавьте файлы проекта.

---
+++ 1.6_SV/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Sample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("Введите натуральное число: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    sum += i;
+                    break;
+                }
+            }
+            Console.WriteLine($"Сумма делителей = {sum}");
+        }
+    }
+}
